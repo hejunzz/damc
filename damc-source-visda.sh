de4e68bedd429dd557cdc0a54f4b0f3bf572dca9
hejunzz <hejunzz@users.noreply.github.com>
2022-10-12 15:37:15 -0700

    update source training script

--- damc-source-visda.sh
+++ damc-source-visda.sh
@@ -1,28 +1,18 @@
 #!/bin/bsh
-echo "Begin Visda DA experiments"
-num_c=12
-src_max_ep=20
-tgt_max_ep=20
-gpu="1"
-task="visda"
-save="save/icml-min04c1-s2021"
-source="train"
-target="validation"
-seed=2021
-interval=0
-bndim=256
-beta=0.01
-alpha=0.3
-mul=1
-smooth=0
-epsilon=0
-thresh=0.4
-modelep=0
-# train --> validation
-file="source"${interval}"-"${task}"-cls"${num_c}"-src"${src_max_ep}"-smooth"${smooth}"-mul"${mul}"-alpha"${alpha}".out"
-echo $file
+echo "Begin pre-training the source model on the Visda-2017 source domain"
+num_c=12        # number of classifiers, for visda the optimal is just the number of categories due to sufficient synthetic source domain 
+src_max_ep=20   # the maximum training epochs, the more the better, and we will take model selection among all the check-points
+gpu="0"         # use which gpu
+task="visda"    # SFUDA task name
+save="save/"    # model check point directory
+source="train"  # souce domain, for visda the data is saved in "$damc_dir/visda/train"
+# target="validation"
+seed=2021   # random seed for reproducing experiment
+bndim=256   # dimension of bottle neck layer
+alpha=0.3   # coefficient of adversarial discrepancy loss
+smooth=0    # 0: doest not use label smoothing
+epsilon=0   # epsilon of label smoothing when smooth==1
+thresh=0.4  # threshold of worst case optimization, for visda we use 0.4
+modelep=0   # 0: from scratch, does not resume from a check-point
 
 python damc_source.py --model_ep $modelep --threshold $thresh --save $save --epsilon $epsilon --mul $mul --src_max_epoch $src_max_ep --smoothing $smooth --tgt_max_epoch $tgt_max_ep --src_alpha $alpha --bn_dim $bndim --gpuid $gpu --task $task --seed $seed --num_c $num_c --source $source --target $target
-
-
-#python damc_source.py --mul $mul --src_max_epoch $src_max_ep --smoothing $smooth --src_alpha $alpha --bn_dim $bndim --gpuid $gpu --task $task --pseudo_interval $interval --pseudo_beta $beta --seed $seed --num_c $num_c --source $source --target $target
